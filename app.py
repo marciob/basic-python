a6cccb44512cad035917facad564b23b43a06c73
m <13090422+marciob@users.noreply.github.com>
2022-09-15 19:08:36 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+print("Mosh Hamedani")
+print("0----")
+print(" ||||")
+print("*" * 10)
+
+price = 10
+price = 20
+print(price)
+
+is_new = True
+
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+print("Hi " + name + ", your favorite is " + color)
+
+birth_year = input("Birth year: ")
+print(type(birth_year))
+age = 2022 - int(birth_year)
+print(type(age))
+print(age)
+
+weight_lbs = input("What is your weight (in pounds)? ")
+weight_kg = float(weight_lbs) * 0.45
+print(weight_kg)
+
+course = "Python's Course for Beginners"
+print(course)
+
+multipleLines = '''
+This is a multiple lines of message,
+it's made by adding text within those three quotes.
+
+'''
+
+print(multipleLines)
+
+print(course[0])
+print(course[0:3])
+print(course[1:5])
+print(course[:5])
+
